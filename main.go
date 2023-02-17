929b3c2dc16d9d3c02e944c303f189514170fae4
Rodolfo Violla <rodolfo.violla@gmail.com>
2023-02-17 16:06:51 -0300

    feat: creates rpc server

--- main.go
+++ main.go
@@ -1,17 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+)
 
 type Item struct {
+	id int
 	title string
 	body string
 }
 
+type API int
+
 var database []Item
 
-func GetByName(title string) (index int, item Item) {
+func getById(id int) (index int, item Item) {
 	for currentIndex, value := range database {
-		if value.title == title {
+		if value.id == id {
 			item = value
 			index = currentIndex
 			break
@@ -20,35 +28,45 @@ func GetByName(title string) (index int, item Item) {
 	return
 }
 
-func AddItems(item []Item) []Item {
+func (api *API) GetItemById(id int, reply *Item) error {
+	_, *reply = getById(id)
+	return nil
+}
+
+func (api *API) AddItems(item []Item, reply *[]Item) error {
 	database = append(database, item...)
-	return item
+	*reply = item
+	return nil
 }
 
-func EditItem(title string, edit Item) Item {
-	index, _ := GetByName(title)
+func (api *API) EditItem(edit Item, reply *Item) error {
+	index, _ := getById(edit.id)
 	database[index] = edit
-	return edit
+	*reply = edit
+	return nil
 }
 
-func DeleteItem(title string) (item Item) {
-	index, item := GetByName(title)
+func DeleteItem(id int, reply *Item) error {
+	var index int
+	index, *reply = getById(id)
 	database = append(database[:index], database[index+1:]...)
-	return 
+	return nil
 }
 
 func main() {
-	fmt.Println("Initial database:", database)
-	a := Item{"first", "a test item"}
-	b := Item{"second", "a second test item"}
-	c := Item{"third", "a third test item"}
-	AddItems([]Item{a, b, c})
-	fmt.Println("Second database:", database)
-	DeleteItem(b.title)
-	fmt.Println("Third database:", database)
-	EditItem("third", Item{"second", "a new second test item"})
-	fmt.Println("Fourth database:", database)
-	_, x := GetByName("second")
-	_, y := GetByName("first")
-	fmt.Println(x, y)
+	var api = new(API)
+	err := rpc.Register(api)
+	if err != nil {
+		log.Fatal("Error registering API", err)
+	}
+	rpc.HandleHTTP()
+	listener, err := net.Listen("tcp", ":4040")
+	if err != nil {
+		log.Fatal("Error creating listener", err)
+	}
+	log.Printf("Serving rpc on port %d", 4040)
+	err = http.Serve(listener, nil)
+	if err != nil {
+		log.Fatal("Error serving:", err)
+	}
 }
